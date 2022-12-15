5d7321da1fd22a80a3d48f5df417dc8146c865f3
DDH <dung.ho@wizeline.com>
2022-12-15 15:40:25 +0700

    Add BaseBadge, BaseButton, BaseCard components

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import { createApp } from 'vue';
 import router from './router';
 import App from './App.vue';
 import store from './store/index';
+import BaseCard from './pages/ui/BaseCard.vue';
+import BaseButton from './pages/ui/BaseButton.vue';
+import BaseBadge from './pages/ui/BaseBadge.vue';
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component('BaseCard', BaseCard);
+app.component('BaseButton', BaseButton);
+app.component('BaseBadge', BaseBadge);
 
 app.mount('#app');
